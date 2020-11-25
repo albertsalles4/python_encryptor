5ff8d9c0b42a70cd2ca91f7c6ac93c254311bd27
AlbertSalles <albertsalles4@gmail.com>
2020-11-25 11:29:08 +0100

    Added salt for custom key

--- Encryptor.py
+++ Encryptor.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
+import base64
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
+
 
 """
 This is a simple python file Encryptor which uses the cryptography library
@@ -8,16 +13,17 @@ class Encryptor():
     
     def __init__(self, key=None):
         self.key = key
+        self.salt = None
     
     def create_key(self):
         self.key = Fernet.generate_key()
         return self.key
     
-    def key_write(self, key_name):
+    def write_key(self, key_name):
         with open(key_name, 'wb') as mykey:
             mykey.write(self.key)
     
-    def key_load(self, key_name):
+    def load_key(self, key_name):
         with open(key_name, 'rb') as mykey:
             self.key = mykey.read()
         return self.key
@@ -54,5 +60,29 @@ class Encryptor():
         with open(decrypted_file, 'wb') as fi:
             fi.write(decrypted)
                         
+    def generate_random_salt(self):
+        self.salt = os.urandom(16)
         
+    def write_salt(self, salt_name):
+        with open(salt_name, 'wb') as mysalt:
+            mysalt.write(self.salt)
+    
+    def load_salt(self, salt_name):
+        with open(salt_name, 'wb') as mysalt:
+            self.salt = mysalt.read()
+        
+    def generate_key_with_password_and_salt(self, password, salt=self.salt):
+        if salt == None:
+            self.generate_random_salt()
+        
+        # We need a bytes-type password, not a string
+        password = bytes(password, 'utf-8') 
+            
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000
+        )
+        self.key = base64.urlsafe_b64encode(kdf.derive(password))
         
